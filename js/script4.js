d29e37ebf9a7e73cf8a7ace1303425bd5408bd69
serena1il <kangchaebin0629@naver.com>
2024-11-05 17:16:35 +0900

    1105

--- js/script4.js
+++ js/script4.js
@@ -29,33 +29,4 @@ $(function () {
             this.classList.toggle("filled"); // 클릭 시 filled 클래스 토글
         });
     });
-
-    // //
-    document.addEventListener("DOMContentLoaded", function () {
-        // Swiper 인스턴스 생성
-        const newSlider = new Swiper(".new-slider", {
-            direction: "horizontal",
-            loop: true,
-            autoplay: {
-                delay: 1000,
-                disableOnInteraction: false,
-            },
-            speed: 1000,
-            effect: "slide",
-            slidesPerView: 1,
-            spaceBetween: 50,
-
-            // Pagination
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-            },
-            // Navigation buttons
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
-        });
-        // newSlider 변수를 활용할 수 있는 추가 코드가 필요할 수 있습니다.
-    });
 });
